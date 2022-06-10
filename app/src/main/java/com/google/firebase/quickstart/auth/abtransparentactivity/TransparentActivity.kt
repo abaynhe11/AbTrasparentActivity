445af6e05d3da77a8c7e4116a32043ac65101d58
Abaynhe Abitew <abaynheabitew@yahoo.com>
2022-06-10 06:26:28 -0500

    A better Transparent Activity code

---
+++ app/src/main/java/com/google/firebase/quickstart/auth/abtransparentactivity/TransparentActivity.kt
@@ -0,0 +1,11 @@
+package com.google.firebase.quickstart.auth.abtransparentactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TransparentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transparent)
+    }
+}
